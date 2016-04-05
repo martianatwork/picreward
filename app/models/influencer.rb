c06b8daad90acb04e99b692fea03a562aa8ae020
arthur bonnecarrere <arthurbonnecarrere@hotmail.fr>
2016-04-05 16:44:27 +0200

    API info OK - need to create new models to store data properly

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -2,4 +2,68 @@ class Influencer < ActiveRecord::Base
   belongs_to :user
   has_many :applications
   mount_uploader :photo, PhotoUploader
+
+  def self.avg_photo_comments(token)
+    total = 0
+    client = Instagram.client(:access_token => token)
+    client.user_recent_media.each do |media_item|
+      total += media_item.comments[:count]
+    end
+    total / 20
+  end
+
+  def self.avg_photo_likes(token)
+    total = 0
+    client = Instagram.client(:access_token => token)
+    client.user_recent_media.each do |media_item|
+      total += media_item.likes[:count]
+    end
+    total / 20
+  end
+
+  def self.top_hashtags(token)
+    hash = {}
+    array = []
+    client = Instagram.client(:access_token => token)
+    client.user_recent_media.each do |media_item|
+      media_item.tags.each do |word|
+        if hash[word]
+          hash[word] += 1
+        else
+          hash[word] = 1
+        end
+      end
+      array << hash.sort_by {|k,v| v}.reverse
+      array[0][0..4]
+    end
+  end
+
+  def self.top_places(token)
+    hash = {}
+    array = []
+    client = Instagram.client(:access_token => token)
+    client.user_recent_media.each do |media_item|
+      if media_item.location
+        media_item.location.name
+        if hash[media_item.location.name]
+          hash[media_item.location.name] += 1
+        else
+          hash[media_item.location.name] = 1
+        end
+      end
+    end
+    # array << hash.sort_by {|k,v| v}.reverse
+    hash
+  end
+
+  def self.username(token)
+    client = Instagram.client(:access_token => token)
+    client.user.username
+  end
+
+
+  def self.followers(token)
+    client = Instagram.client(:access_token => token)
+    client.user.counts.followed_by
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ActiveRecord::Base
+  include InfluencersHelper
+  after_create :initialize_influencer
   # Include default devise modules. Others available are:
   devise :omniauthable, omniauth_providers: [:instagram]
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+def initialize_influencer
+  Influencer.create(
+  user_id: self.id,
+  avg_photo_comments: Influencer.avg_photo_comments(self.token),
+  avg_photo_likes: Influencer.avg_photo_likes(self.token),
+  top_hashtags: Influencer.top_hashtags(self.token),
+  top_places: Influencer.top_places(self.token),
+  followers: Influencer.followers(self.token),
+  username: Influencer.username(self.token)
+  )
+      # influencer.save!
+end
 
 def self.find_for_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
