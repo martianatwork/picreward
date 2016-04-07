5ee38043ef3b1bba5b451861f65b7e6e48da832b
arthur bonnecarrere <arthurbonnecarrere@hotmail.fr>
2016-04-07 10:02:21 +0200

    fixing sign up for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ActiveRecord::Base
   include InfluencersHelper
-  after_create :initialize_influencer
+  # after_create :initialize_influencer
   # Include default devise modules. Others available are:
-  devise :omniauthable, omniauth_providers: [:instagram]
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:instagram]
 
-def initialize_influencer
-  influencer = Influencer.create(
-  user_id: self.id,
-  avg_photo_comments: Influencer.avg_photo_comments(self.token),
-  avg_photo_likes: Influencer.avg_photo_likes(self.token),
-  followers: Influencer.followers(self.token),
-  username: Influencer.username(self.token)
-  )
+# def initialize_influencer
+#   influencer = Influencer.create(
+#   user_id: self.id,
+#   avg_photo_comments: Influencer.avg_photo_comments(self.token),
+#   avg_photo_likes: Influencer.avg_photo_likes(self.token),
+#   followers: Influencer.followers(self.token),
+#   username: Influencer.username(self.token)
+#   )
 
-  influencer.top_hashtags(token)
-  influencer.top_places(token)
+#   influencer.top_hashtags(token)
+#   influencer.top_places(token)
       # influencer.save!
-end
+# end
 
 def self.find_for_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -31,6 +31,17 @@ def self.find_for_oauth(auth)
       user.email = auth.info.nickname + "@example.com"
       user.save!
     end
+    # influencer = Influencer.create(
+    #   user_id: 5,
+    #   avg_photo_comments: Influencer.avg_photo_comments(self.token),
+    #   avg_photo_likes: Influencer.avg_photo_likes(self.token),
+    #   followers: Influencer.followers(self.token),
+    #   username: Influencer.username(self.token)
+    #   )
+
+    #   influencer.top_hashtags(self.token)
+    #   influencer.top_places(self.token)
+    #   infleuncer.save!
   end
 
 end
