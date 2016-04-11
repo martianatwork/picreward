4b4ed2a915f67410b8ca3248159bc00a22553eec
arthur bonnecarrere <arthurbonnecarrere@hotmail.fr>
2016-04-12 01:17:02 +0200

    button changes and started to work on JS

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,2 +1,30 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var page_content = $.ajax({
+   url: 'http://localhost:3000/influencers/13',
+   type:'GET',
+   success: function(data){
+    // console.log(data);
+      response = $($.parseHTML(data)).filter(".wrapper-influencer-show");
+      // console.log(hello);
+   }
+});
+
+
+$(document).ready(function() {
+$('.insta-pic-business-show').on('click', function(){
+  vex.defaultOptions.className = 'vex-theme-default';
+  vex.open({
+  content: response,
+  contentCSS: {width: '1200px',
+  position:'absolute',
+  // 'margin': 'auto',
+  top:'10px',
+  left:'40px',
+},
+  });
+});
+
+});
+
+
