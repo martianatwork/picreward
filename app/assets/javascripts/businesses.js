a74a7becbd1e5e67745b56c22e6f63aee7d1985e
arthur bonnecarrere <arthurbonnecarrere@hotmail.fr>
2016-04-12 16:00:54 +0200

    developping javasciprt show for influencer

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,30 +1,32 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var page_content = $.ajax({
-   url: 'http://localhost:3000/influencers/13',
-   type:'GET',
-   success: function(data){
-    // console.log(data);
-      response = $($.parseHTML(data)).filter(".wrapper-influencer-show");
-      // console.log(hello);
-   }
-});
-
 
 $(document).ready(function() {
-$('.insta-pic-business-show').on('click', function(){
-  vex.defaultOptions.className = 'vex-theme-default';
-  vex.open({
-  content: response,
-  contentCSS: {width: '1200px',
-  position:'absolute',
-  // 'margin': 'auto',
-  top:'10px',
-  left:'40px',
-},
-  });
-});
+  $('.show-influencer').on('click', function(elem){
+    console.log(elem);
+    var infl_id = $(elem.target).data("id");
+    vex.defaultOptions.className = 'vex-theme-default';
 
+    $.ajax({
+     url: 'http://localhost:3000/influencers/' + infl_id,
+     type:'GET',
+     success: function(data){
+      // console.log(data);
+        response = $($.parseHTML(data)).filter(".business-card-container");
+        vex.open({
+          content: response,
+          contentCSS: {
+            width: '800px',
+            // position:'absolute',
+            // 'margin': 'auto',
+            // top:'10px',
+            // left:'40px',
+          },
+        });
+        // console.log(hello);
+     }
+    });
+  });
 });
 
 
