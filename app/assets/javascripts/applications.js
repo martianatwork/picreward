5b73ed66989232f0630836dda986849bf17ab20b
Alexis Agostini <alexisagos@gmail.com>
2016-09-24 19:09:23 +0200

    changed asset pipeline

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -38,3 +38,16 @@ $(document).ready(function() {
     $(this).addClass("selected");
   });
 });
+
+// Smooth-scroll
+function scrollToElement (selector) {
+  $('html, body').animate({
+    scrollTop: $(selector).offset().top
+  }, 1000);
+};
+
+$(document).on('click', 'a.link-details', function (e) {
+  e.preventDefault();
+  scrollToElement($(this).attr('href'));
+});
+
